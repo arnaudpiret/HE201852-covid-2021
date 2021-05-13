d03a6e54fd5073dc12fbfe2f7ed1d15376e74024
arnaudpiret <67874660+arnaudpiret@users.noreply.github.com>
2021-05-14 01:07:41 +0200

    Create commun-webservice-villeList.sql

---
+++ backend/scripts-api-rest/commun-webservice-villeList.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE "DBA"."proc_villeList"()
+BEGIN    
+    call sa_set_http_header( 'Content-Type', 'text/html' );
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+	  select* from tbvilles ORDER BY  villeName asc;
+
+END
+
+
+/******************************************/
+CREATE SERVICE "initialiser" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_villeList();
