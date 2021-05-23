fc2d11fa37924024aa0ea3f6daa5919dbc85a76c
arnaudpiret <67874660+arnaudpiret@users.noreply.github.com>
2021-05-23 03:11:48 +0200

    Update ville.js

--- frontend/js/ville.js
+++ frontend/js/ville.js
@@ -11,12 +11,14 @@ function chargerlisteVille(){
             listeville = JSON.parse(xhr.responseText);	
         };
     xhr.send(); 
-					let stringlisteville="<table id=\"tableville\"><tbody>";
-				if (listeville.length>2){stringlisteville+="<tr><th>codePostal</th><th>Nom</th></tr>"}
-				for (let i=0;i<listeville.length;i++){
-					stringlisteville+="<tr><td>"+listeville[i].CodeP+"</td><td>"+listeville[i].villeName+"</td></tr>"
-				}
-				document.querySelector("#zoneListeVille").innerHTML = stringlisteville+"</tbody></table>"
+	let stringlisteville="<table id=\"tableville\"><tbody>";
+	if (listeville.length>2){stringlisteville+="<tr><th>codePostal</th><th>Nom</th></tr>"}
+		for (let i=0;i<listeville.length;i++){
+			stringlisteville+="<tr><td>"+listeville[i].CodeP+"</td><td>"+listeville[i].villeName+"</td></tr>"
+		}
+	document.querySelector("#zoneListeVille").innerHTML = stringlisteville+"</tbody></table>";
+	loadMaladeVilles();
+	insertoption();
 }
 //US02
 function ajouterVille(formville){
@@ -31,13 +33,21 @@ function ajouterVille(formville){
 	if (erreur==0){
 		
 		let xhr = new XMLHttpRequest();                                                             
-    xhr.open('get', "insertville?CP="+document.getElementById("formAjoutVille").codePostal.value+"&VillN="+document.getElementById("formAjoutVille").ville.value, true);    
-      xhr.onload =                                                                            
-
+    xhr.open('get', "insertville?CP="+document.getElementById("formAjoutVille").codePostal.value+"&VillN="+document.getElementById("formAjoutVille").ville.value, true);                                                                               
     xhr.send();
 		document.getElementById("tableville").innerHTML = document.getElementById("tableville").innerHTML.substring(0,document.getElementById("tableville").innerHTML.length-8)+"<tr><td>"+formville.codePostal.value+"</td><td>"+formville.nomVille.value+"</td></tr></tbody>";
 		listeville.push({CodeP:Number(formville.codePostal.value),villeName: formville.nomVille.value });
 	
 	};
+	
 	return false;
+}
+// ajout option dans formulaire
+let villehtmloption=''
+window.setTimeout(addVilleOption,1000)
+function addVilleOption(){
+	for ( i=0; i < listeville.length ;i++){
+	villehtmloption+= "<option value=\""+listeville[i].CodeP+"\">"+listeville[i].villeName+" ["+listeville[i].CodeP+"]</option>"
+	}
+	document.getElementById("villeVisitee").innerHTML = villehtmloption
 }
